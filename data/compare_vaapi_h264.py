c36e77004e17edcfd9664968614a9603d9bbf1b9
Anton Schubert <ischluff@mailbox.org>
2020-01-16 00:09:59 +0100

    add vaapi vs software-codec comparison, extend util

--- data/compare_vaapi_h264.py
+++ data/compare_vaapi_h264.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 import util
 
-# Reference video
-ref = "reference.mkv"
-
 # Should return a dict of formats
 # Each format should be a dict containing ffmpeg options like:
 # {"opts": "-i ..."}
@@ -12,9 +9,9 @@ ref = "reference.mkv"
 def formats():
     ret = {}
 
-    # Use a 1:1 copy to check whether the input itself reaches a score of 100
+    # Use a 1:1 copy to check whether the input itself reaches a score close to 100
     # otherwise there's some problems with your reference content
-    #ret["ref"] = {"opts": f"-i {ref} -c:v copy"}
+    #ret["ref"] = {"opts": "-i $ref -c:v copy"}
 
     # generate formats for different bitrates
     for rate in [3000, 9000, 15000]:
@@ -24,7 +21,7 @@ def formats():
             "opts": f"""
     -vaapi_device /dev/dri/renderD128
     -hwaccel vaapi -hwaccel_output_format vaapi
-    -i {ref}
+    -i $ref
     -vf 'format=nv12|vaapi,hwupload'
     -c:v h264_vaapi -compression_level 1
     -b:v {rate}k -maxrate:v {rate}k -bufsize {rate*2}k
@@ -34,7 +31,7 @@ def formats():
             "encoder": "x264",
             "rate": rate,
             "opts": f"""
-    -i {ref}
+    -i $ref
     -c:v libx264 -preset:v veryslow -crf:v 18
     -b:v {rate}k -maxrate:v {rate}k -bufsize {rate*2}k
 """}
@@ -65,29 +62,5 @@ def plot(formats, scores):
     plt.savefig("bitrates.pdf")
     plt.show()
 
-def main():
-    import argparse
-    import json
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--task", choices=["transcode", "plot"],
-        help="do only some of the tasks", default="all")
-
-    args = parser.parse_args()
-    fmts = formats()
-
-    # transcode creates subformats and calculates scores, speed and actual rate
-    if args.task == "all" or args.task == "transcode":
-        scores = util.transcode(ref, fmts, scale=None)
-        with open("tmp/scores.json", "w") as f:
-            json.dump(scores, f)
-
-    # do plots
-    scores = None
-    if args.task == "all" or args.task == "plot":
-        with open("tmp/scores.json", "r") as f:
-            scores = json.load(f)
-        plot(fmts, scores)
-
 if __name__ == "__main__":
-    main()
+    util.init(formats(), plot)
--- data/util.py
+++ data/util.py
@@ -1,4 +1,6 @@
+import argparse
 import os
+import math
 import json
 import shlex, subprocess
 import re
@@ -40,19 +42,36 @@ ffmpeg -y -hide_banner -v warning
 
     with open(scorepath, "r") as f:
         data = json.load(f)
-        return data["VMAF score"]
 
-def transcode(ref, formats, scale=None):
+        return [frame["metrics"]["vmaf"] + 1 for frame in data["frames"]]
+
+def transcode(ref, formats, scale=None, tmpdir="tmp"):
     os.makedirs("tmp", exist_ok=True)
-    scores = {}
+    results = {}
+
+    # prepare reference (if this is not yuv the scores are strange...)
+    refpath = os.path.join(tmpdir, "ref.nut")
+    cmd = f"""
+ffmpeg -y -hide_banner -v warning
+-i {ref}
+-c:v rawvideo -an
+{refpath}
+"""
+    try:
+        subprocess.check_call(shlex.split(cmd))
+    except subprocess.CalledProcessError:
+        print(f"Failed to decode ref '{ref}'")
+        return results
+
+    progresspath = os.path.join(tmpdir, "progress")
     for fmt in formats:
-        scores[fmt] = {}
+        results[fmt] = {}
 
         # encode input
-        codedpath = os.path.join("tmp", f"{fmt}.mkv")
+        codedpath = os.path.join(tmpdir, f"{fmt}.nut")
         cmd = f"""
-ffmpeg -y -hide_banner -v warning -stats -progress tmp/progress
-{formats[fmt]["opts"]}
+ffmpeg -y -hide_banner -v warning -stats -progress {progresspath}
+{formats[fmt]["opts"].replace("$ref", refpath)}
     -an
     {codedpath}
 """
@@ -64,23 +83,59 @@ ffmpeg -y -hide_banner -v warning -stats -progress tmp/progress
 
         # read final speed from progress
         speed = 0
-        with open("tmp/progress", "r") as f:
+        with open(progresspath, "r") as f:
             progress = f.read().split("progress=continue")[-1]
             for line in progress.split("\n"):
                 match = re.match("^speed=([\d.]+)x$", line)
                 if match is not None:
-                    scores[fmt]["speed"] = float(match[1])
+                    results[fmt]["speed"] = float(match[1])
 
         # probe real coded bitrate
-        scores[fmt]["rate"] = probe_rate(codedpath)
+        results[fmt]["rate"] = probe_rate(codedpath)
 
         # calculate vmaf score
-        score = calc_score(ref, codedpath, scale)
-        if score is None:
+        scores = calc_score(ref, codedpath, scale)
+        if scores is None:
             print(f"Failed to score {fmt}")
             continue
 
-        scores[fmt]["score"] = score
+        # Calculate different aggregates
+        score_mean = sum(scores) / len(scores)
+        print("Mean:", score_mean)
+        results[fmt]["score_mean"] = score_mean
+
+        score_harm_mean = len(scores) / sum(1 / (score + 1) for score in scores) - 1
+        print("Harmonic mean:", score_harm_mean)
+        results[fmt]["score_harm_mean"] = score_harm_mean
+
+        score_10th_pct = sorted(scores)[math.ceil(0.1*len(scores))]
+        print("10th pctile:", score_10th_pct)
+        results[fmt]["score_10th_pct"] = score_10th_pct
+
+        print("Min:", min(scores))
+        results[fmt]["score_min"] = min(scores)
+
+    return results
+
+def init(formats, plot):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--task", choices=["transcode", "plot"],
+        help="do only some of the tasks", default="all")
+    parser.add_argument("ref", help="reference video")
+
+    args = parser.parse_args()
+
+    # transcode creates subformats and calculates scores, speed and actual rate
+    if args.task == "all" or args.task == "transcode":
+        scores = util.transcode(args.ref, formats, scale=None)
+        with open("tmp/scores.json", "w") as f:
+            json.dump(scores, f)
+
+    # do plots
+    scores = None
+    if args.task == "all" or args.task == "plot":
+        with open("tmp/scores.json", "r") as f:
+            scores = json.load(f)
+        plot(formats, scores)
 
 
-    return scores
