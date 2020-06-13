c9b11dc1790e74f23bd5bb55052e968ec7127f49
Anton Schubert <ischluff@mailbox.org>
2020-06-13 21:47:39 +0200

    be less strict about scores when testing

--- data/test/test_profile.py
+++ data/test/test_profile.py
@@ -14,7 +14,7 @@ class TestSimpleProfile(unittest.TestCase):
 
     def test_process(self):
         reference = path.join(basedir, "fixtures/reference.nut")
-        results = [{'reference': 'reference', 'profile': 'simple', 'tag': 'testing', 'codec': 'copy', 'opts': '-i $ref -c:v copy', 'speed': 89.6, 'rate': 116135.56, 'score_mean': 99.26749500000001, 'score_harm_mean': 99.254729088278, 'score_10th_pct': 98.49073, 'score_min': 98.42804}, {'reference': 'reference', 'profile': 'simple', 'tag': 'testing', 'codec': 'libvpx-vp9', 'opts': '-i $ref -c:v libvpx-vp9', 'speed': 1.04, 'rate': 81.973, 'score_mean': 85.56661600000001, 'score_harm_mean': 82.5296607669634, 'score_10th_pct': 73.33489, 'score_min': 51.29036}, {'reference': 'reference', 'profile': 'simple', 'tag': 'testing', 'codec': 'libx264', 'opts': '-i $ref -c:v libx264', 'speed': 4.06, 'rate': 224.868, 'score_mean': 98.03638800000002, 'score_harm_mean': 97.99567274453702, 'score_10th_pct': 96.49025, 'score_min': 96.38498}, {'reference': 'reference', 'profile': 'simple', 'tag': 'testing', 'codec': 'libx265', 'opts': '-i $ref -c:v libx265', 'speed': 2.02, 'rate': 78.706, 'score_mean': 96.59885399999999, 'score_harm_mean': 96.53314263881514, 'score_10th_pct': 94.4406, 'score_min': 94.04528}]
+        results = [{'reference': 'reference', 'profile': 'simple', 'tag': 'testing', 'codec': 'copy', 'opts': '-i $ref -c:v copy', 'speed': 89.6, 'rate': 116135.56, 'score_mean': 99.2674, 'score_harm_mean': 99.2547, 'score_10th_pct': 98.49073, 'score_min': 98.428}, {'reference': 'reference', 'profile': 'simple', 'tag': 'testing', 'codec': 'libvpx-vp9', 'opts': '-i $ref -c:v libvpx-vp9', 'speed': 1.04, 'rate': 81.973, 'score_mean': 85.5666, 'score_harm_mean': 82.5296, 'score_10th_pct': 73.3349, 'score_min': 51.29036}, {'reference': 'reference', 'profile': 'simple', 'tag': 'testing', 'codec': 'libx264', 'opts': '-i $ref -c:v libx264', 'speed': 4.06, 'rate': 224.868, 'score_mean': 98.0363, 'score_harm_mean': 97.9956, 'score_10th_pct': 96.4902, 'score_min': 96.38498}, {'reference': 'reference', 'profile': 'simple', 'tag': 'testing', 'codec': 'libx265', 'opts': '-i $ref -c:v libx265', 'speed': 2.02, 'rate': 78.706, 'score_mean': 96.5989, 'score_harm_mean': 96.5331, 'score_10th_pct': 94.4406, 'score_min': 94.04528}]
 
         for got, expected in zip(self.profile.process(reference, "testing", self.tmpdir), results):
             # sometimes we do not get a speed?
@@ -22,7 +22,13 @@ class TestSimpleProfile(unittest.TestCase):
                 del got["speed"]
 
             del expected["speed"]
-            self.assertEqual(got, expected, msg=f"result for {expected['codec']} changed")
+
+            for key in expected:
+                msg = f"{key} for {expected['codec']} changed: expected {expected[key]}, {got[key]}"
+                if type(expected[key] == float):
+                    self.assertAlmostEqual(got[key], expected[key], delta=1, msg=msg)
+                else:
+                    self.assertEqual(got[key], expected[key], msg=msg)
 
     def tearDown(self):
         shutil.rmtree(self.tmpdir, ignore_errors=True)
