fb9f299dcaf5e07e26cd10b92f0e0d12825a9022
Anton Schubert <ischluff@mailbox.org>
2020-12-12 00:43:03 +0100

    add comments to simple profile

--- data/profiles/simple.py
+++ data/profiles/simple.py
@@ -2,10 +2,12 @@ from libquality.profile import Profile as Base
 
 
 class Profile(Base):
+    """Example profile for testing and demonstration"""
     name = "simple"
     dimensions = ["codec"]
 
     def formats(self):
+        """Returns formats to encode"""
         for codec in ["copy", "libvpx-vp9", "libx264", "libx265"]:
             yield {
                 "codec": codec,
@@ -13,4 +15,5 @@ class Profile(Base):
             }
 
     def plot(self, df, plotdir):
+        """Plots scores and stores them in plotdir"""
         df.plot()
